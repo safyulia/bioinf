65b419b978e22c651bf45a8abdbe94f72370ccb9
safyulia <60537547+safyulia@users.noreply.github.com>
2021-04-26 09:18:06 +0300

    Add files via upload

---
+++ HW13/2_paired.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+from rpy2 import robjects
+from rpy2.robjects import Formula
+
+from rpy2.robjects import pandas2ri
+pandas2ri.activate()
+
+from rpy2.robjects.packages import importr
+
+base = importr("base")
+print(base._libPaths())
+stats = importr("stats")
+DESeq2 = importr("DESeq2")
+
+# Load read counts table
+counts = pd.read_csv("colon_cancer_tumor_vs_normal_paired_counts.tsv.txt", sep="\t", index_col=0)
+
+# Define meta
+meta = pd.DataFrame({"Tissue": ["Tumor"]*5 + ["Normal"]*5, "patient_number": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5] }, index=counts.columns)
+meta["Tissue"] = stats.relevel(robjects.vectors.FactorVector(meta["Tissue"]), ref="Normal")
+
+# Calculate normalization factors
+dds = DESeq2.DESeqDataSetFromMatrix(countData=counts, colData=meta, design=Formula("~ patient_number + Tissue"))
+dds = DESeq2.DESeq(dds)
+
+res = DESeq2.results(dds, name="Tissue_Tumor_vs_Normal")
+res = DESeq2.lfcShrink(dds, coef="Tissue_Tumor_vs_Normal", type="apeglm")
+res = pd.DataFrame(base.as_data_frame(res))
+res.index = counts.index
+res = res.sort_values("padj")
+res = res.loc[res["padj"] < 0.05]
+res = res.loc[res["log2FoldChange"].abs() >= 1]
+
+print("топ 10:", res.iloc[:10].index)
+print("Число дифференциально экспрессированных генов:", len(res))
+
+res.to_csv("DESeq2_results_paired.tsv", sep="\t")
